a65da7d706b56db15d7166f3f40bde1c11731f25
steak <github@hastake.com>
2013-08-09 20:05:37 +0200

    [ref] use processing.js as js api only

--- sharky/hamapp.js
+++ sharky/hamapp.js
@@ -1,3 +1,29 @@
+canvas = function() {
+	var data = {
+		x: 100,
+		y: 100,
+		width: 300,
+		height: 200,
+	};
+
+	return {
+		draw: function(el) {
+			var p = new Processing(el);
+			p.size(data.width, data.height);
+			p.background("#ffaaaa");
+			p.smooth();
+			p.stroke(50);
+			p.noLoop();
+			p.ellipse(data.x, data.y, 50, 50);
+		},
+
+		move: function(nx, ny) {
+			data.x = nx;
+			data.y = ny;
+		}
+	};
+}();	
+
 var hamapp = Backbone.View.extend({
 	initialize: function() {
 		this.template = Handlebars.compile($("#main-page").html());
@@ -14,6 +40,8 @@ var hamapp = Backbone.View.extend({
 	render: function() {
 		var html = this.template(this.variables);
 		$(this.el).html(html);
+		if (this.variables.display_canvas)
+			canvas.draw(document.getElementById('render-zone'));
 	},
 
 	events: {
@@ -25,6 +53,6 @@ var hamapp = Backbone.View.extend({
 		"click #show-canvas": function() {
 			this.variables.display_canvas = true;
 			this.render();
-		}
+		},
 	}
 });
