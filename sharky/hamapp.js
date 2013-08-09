32d0e2e3725e49277a73225767a196397c39c965
steak <github@hastake.com>
2013-08-09 19:11:53 +0200

    [enh] show/mask canvas

--- sharky/hamapp.js
+++ sharky/hamapp.js
@@ -1,17 +1,30 @@
 var hamapp = Backbone.View.extend({
 	initialize: function() {
 		this.template = Handlebars.compile($("#main-page").html());
+		this.variables = {
+			"state": 1,
+			"display_canvas": true,
+			"help-title": "Introduction",
+			"help-text": "wazup lorem ipsum",
+			"print-debug": true,
+		};
 		this.render();
 	},
 
 	render: function() {
-		var html = this.template({
-			"state": 1,
-			"display-canvas": true,
-			"help-title": "Introduction",
-			"help-text": "wazup lorem ipsum",
-			"print-debug": true,
-		});
+		var html = this.template(this.variables);
 		$(this.el).html(html);
+	},
+
+	events: {
+		"click #mask-canvas": function() {
+			this.variables.display_canvas = false;
+			this.render();
+		},
+
+		"click #show-canvas": function() {
+			this.variables.display_canvas = true;
+			this.render();
+		}
 	}
 });
